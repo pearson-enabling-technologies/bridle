8a509d20e7a9a565299bf98b21a532e52b3d8724
Dario Villanueva <dario.villanueva@gmail.com>
2013-05-22 10:19:49 +0100

    fixed bridle global

--- src/header.js
+++ src/header.js
@@ -9,6 +9,10 @@ if (typeof window.define === "function" && window.define.amd) {
   window.define("Bridle", ['jquery', 'd3'], function() {
     return Bridle;
   });
+// we're not using amd, then set up as 
+// global var
+} else {
+  window.Bridle = Bridle;
 }
 
 
