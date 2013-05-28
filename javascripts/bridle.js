310c855e998c60301f9d7ff559080e4f0f65ae89
Dario Villanueva <dario.villanueva@gmail.com>
2013-05-29 00:35:49 +0100

    new version

--- javascripts/bridle.js
+++ javascripts/bridle.js
@@ -970,7 +970,7 @@ Bridle.LineChart = function() {
           })
 
       // add points
-      var points = gSeriesEnter.append("g")
+      var circles = gSeriesEnter.append("g")
         .attr("class", "circles")
         .selectAll('circle')
         .data(function(d) {
@@ -981,7 +981,7 @@ Bridle.LineChart = function() {
         })
 
       // add the points
-      var circlesEnter = points.enter();
+      var circlesEnter = circles.enter();
 
       circlesEnter.append('circle')
         .attr("opacity", 0.1)
