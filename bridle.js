2d7c9fc2d397fdd716aaadaefab5cf5b69a3b4c9
Dario Villanueva <dario.villanueva@gmail.com>
2013-05-28 10:14:31 +0100

    make bridle export name lowercase

--- bridle.js
+++ bridle.js
@@ -1897,7 +1897,7 @@ function merge(left,right,comparison)
 };
 // add support for amd
 if (typeof window.define === "function" && window.define.amd) {
-  window.define("Bridle", ['jquery', 'd3'], function() {
+  window.define("bridle", ['jquery', 'd3'], function() {
     return Bridle;
   });
 // we're not using amd, then set up as 
