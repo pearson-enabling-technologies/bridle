c51ee73836b16518a669e068180fde5d1c0143d0
Dario Villanueva <dario.villanueva@gmail.com>
2013-06-12 17:47:44 +0100

    fixed circle issue

--- bridle.js
+++ bridle.js
@@ -1003,7 +1003,7 @@ Bridle.LineChart = function() {
         .data(function(d) {
           return d.values;
         }, function(d) {
-          return d.z;
+          return xValue(d);
         });
         
       // add the points
