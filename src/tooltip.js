17aa8a3aec5fdef11d9080b29c3a50180df791f7
Marek Stasikowski <marek.stasikowski@gmail.com>
2014-03-26 16:57:18 +0000

    satisfy JSHint

--- src/tooltip.js
+++ src/tooltip.js
@@ -4,7 +4,7 @@
 
 Bridle.tooltip = {
 
-  show: function(pos, content, gravity, dist) {
+  show : function(pos, content, gravity, dist) {
     var container = $('<div class="bridle tooltip">');
 
     gravity = gravity || 's';
@@ -15,17 +15,16 @@ Bridle.tooltip = {
 
     container
       .html(content)
-      .css({left: -1000, top: -1000, opacity: 0})
+      .css({left : -1000, top : -1000, opacity : 0})
       .appendTo(bridleContainer);
 
-    var height = container.height() + parseInt(container.css('padding-top'))  + parseInt(container.css('padding-bottom')),
-        width = container.width() + parseInt(container.css('padding-left'))  + parseInt(container.css('padding-right')),
-        windowWidth = $(window).width(),
-        windowHeight = $(window).height(),
-        scrollTop = $('body').scrollTop(),  //TODO: also adjust horizontal scroll
-        left, top;
-
-
+    var height =
+        container.height() + parseInt(container.css('padding-top')) + parseInt(container.css('padding-bottom')),
+      width = container.width() + parseInt(container.css('padding-left')) + parseInt(container.css('padding-right')),
+      windowWidth = $(window).width(),
+      windowHeight = $(window).height(),
+      scrollTop = $('body').scrollTop(),  //TODO: also adjust horizontal scroll
+      left, top;
 
     //TODO: implement other gravities
     switch (gravity) {
@@ -34,27 +33,38 @@ Bridle.tooltip = {
       case 'n':
         left = pos[0] - (width / 2);
         top = pos[1] + dist;
-        if (left < 0) left = 5;
-        if (left + width > windowWidth) left = windowWidth - width - 5;
-        if (scrollTop + windowHeight < top + height) top = pos[1] - height - dist;
+        if (left < 0) {
+          left = 5;
+        }
+        if (left + width > windowWidth) {
+          left = windowWidth - width - 5;
+        }
+        if (scrollTop + windowHeight < top + height) {
+          top = pos[1] - height - dist;
+        }
         break;
       case 's':
         left = pos[0] - (width / 2);
         top = pos[1] - height - dist;
-        if (left < 0) left = 5;
-        if (left + width > windowWidth) left = windowWidth - width - 5;
-        if (scrollTop > top) top = pos[1] + dist;
-
+        if (left < 0) {
+          left = 5;
+        }
+        if (left + width > windowWidth) {
+          left = windowWidth - width - 5;
+        }
+        if (scrollTop > top) {
+          top = pos[1] + dist;
+        }
 
         break;
     }
 
     container
-        .css({
-          left: left,
-          top: top,
-          opacity: 1
-        });
+      .css({
+        left    : left,
+        top     : top,
+        opacity : 1
+      });
   },
 
   cleanup : function() {
@@ -62,16 +72,16 @@ Bridle.tooltip = {
 
     // remove right away, but delay the show with css
     tooltips.css({
-        'transition-delay': '0 !important',
-        '-moz-transition-delay': '0 !important',
-        '-webkit-transition-delay': '0 !important'
+      'transition-delay'         : '0 !important',
+      '-moz-transition-delay'    : '0 !important',
+      '-webkit-transition-delay' : '0 !important'
     });
 
-    tooltips.css('opacity',0)
+    tooltips.css('opacity', 0);
 
     setTimeout(function() {
-      tooltips.remove()
+      tooltips.remove();
     }, 500);
   }
 
-}
+};
